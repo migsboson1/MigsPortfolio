811f56b6169a396cb36703df15592add1c01e991
Miguel Jr <miguelaguirre93@icloud.com>
2026-08-31 21:09:51 -0700

    still fixing bug card fip

--- assets/javascript/experience.js
+++ assets/javascript/experience.js
@@ -1,5 +1,5 @@
-const card1 = document.getElementById("card1")
+const card = document.getElementById("card1")
 
-card1.addEventListener("click", function() {
-    card1.classList.toggle("flipped");
+card.addEventListener("click", function() {
+    card.classList.toggle("flipped");
 });
