dd2b7ba009fe250462199d8f3b39f3cefdd93598
Miguel Jr <miguelaguirre93@icloud.com>
2026-08-31 22:44:23 -0700

    updated css and js

--- assets/javascript/experience.js
+++ assets/javascript/experience.js
@@ -3,3 +3,9 @@ const card = document.getElementById("card1")
 card.addEventListener("click", function() {
     card.classList.toggle("flipped");
 });
+
+const card2 = document.getElementById("card2")
+
+card2.addEventListener("click", function(){
+    card2.classList.toggle("flipped")
+})
