1ba0100fea20c82b117aacab916632c5fc3fc655
Miguel Jr <miguelaguirre93@icloud.com>
2026-08-31 22:55:12 -0700

    fixed multiple bugswhen adding more cards

--- assets/javascript/experience.js
+++ assets/javascript/experience.js
@@ -1,11 +1,6 @@
-const card = document.getElementById("card1")
+const card = document.getElementById("flipped-card")
 
 card.addEventListener("click", function() {
     card.classList.toggle("flipped");
 });
 
-const card2 = document.getElementById("card2")
-
-card2.addEventListener("click", function(){
-    card2.classList.toggle("flipped")
-})
